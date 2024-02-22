71eb99c1cdcc7804ad0520df9839eeaeaadb90aa
eliasroebl <eliasroebl@gmail.com>
2024-02-22 19:56:04 +0100

    Lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MapComponent} from "./map/map.component";
-import {LayerConfigStepperComponent} from "./layer-config-stepper/layer-config-stepper.component";
+import { MapComponent } from './map/map.component';
+import { LayerConfigStepperComponent } from './layer-config-stepper/layer-config-stepper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MapComponent, LayerConfigStepperComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'permacultr';

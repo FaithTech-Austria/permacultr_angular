7929a76b06d26e29595092efc895af98d98a6dcc
eliasroebl <eliasroebl@gmail.com>
2024-02-22 19:54:06 +0100

    Add esLint and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       zIndex: {
-        'max': 1000
-      }
+        max: 1000,
+      },
+      width: {
+        100: '25rem',
+        104: '26rem',
+        108: '27rem',
+        112: '28rem',
+        116: '29rem',
+        120: '30rem',
+      },
     },
   },
   plugins: [],
-}
+};

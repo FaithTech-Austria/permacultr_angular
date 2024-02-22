a1c23aebd278bce4b085a666c184d2deb94ccac7
eliasroebl <eliasroebl@gmail.com>
2024-02-22 19:56:17 +0100

    Add Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProjectPortalComponent } from './layer-config-stepper/project-portal/project-portal.component';
+import { ProjectFormComponent } from './layer-config-stepper/project-form/project-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'start', component: ProjectPortalComponent },
+  { path: 'create-project', component: ProjectFormComponent },
+  { path: '', redirectTo: 'start', pathMatch: 'full' },
+];

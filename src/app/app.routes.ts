8c3b08835412c3eec5e7c317c9d929016db881d6
eliasroebl <eliasroebl@users.noreply.github.com>
2024-02-22 20:44:28 +0000

    deploy: 31ecf5972b95918a0fcb59a4026fef2d9d34d71c

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProjectPortalComponent } from './layer-config-stepper/project-portal/project-portal.component';
-import { ProjectFormComponent } from './layer-config-stepper/project-form/project-form.component';
-
-export const routes: Routes = [
-  { path: 'start', component: ProjectPortalComponent },
-  { path: 'create-project', component: ProjectFormComponent },
-  { path: '', redirectTo: 'start', pathMatch: 'full' },
-];

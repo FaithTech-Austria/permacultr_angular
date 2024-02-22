8c08ec85d293acc274e1514fde89d287780a6ace
eliasroebl <eliasroebl@gmail.com>
2024-02-22 19:57:41 +0100

    Further Lint

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,39 +1,25 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import L, {marker} from "leaflet";
-import {MapService} from "../map-service/map.service";
-import {MatButton} from "@angular/material/button";
-import {MapUtilsOverlayComponent} from "./map-utils-overlay/map-utils-overlay.component";
+import { AfterViewInit, Component } from '@angular/core';
+import { MapService } from '../map-service/map.service';
+import { MatButton } from '@angular/material/button';
+import { MapUtilsOverlayComponent } from './map-utils-overlay/map-utils-overlay.component';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [
-    MatButton,
-    MapUtilsOverlayComponent
-  ],
+  imports: [MatButton, MapUtilsOverlayComponent],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrl: './map.component.css',
 })
-export class MapComponent implements AfterViewInit{
-  constructor(
-    private mapService: MapService
-  ) { }
-
+export class MapComponent implements AfterViewInit {
+  constructor(protected mapService: MapService) {}
 
   public ngAfterViewInit() {
-
-    this.mapService.initializeMap('map', [ -6.1309975, 35.954609 ], 17)
-
-
-
-/*    const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-      maxZoom: 20,
-      subdomains:['mt0','mt1','mt2','mt3']
-    });
-    googleSat.addTo(map)*/
-  }
-
-  doStuff() {
-    this.mapService.startDrawingPolygon()
+    this.mapService.initializeMap('map', [-6.1309975, 35.954609], 17);
   }
 }
+
+// TODO Create Project => Name, Description, Polygon => areaOfInterest
+// TODO BaseLayer:
+// TODO
+// TODO
+// TODO
